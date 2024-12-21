e3f5d49657d4d1e0f67c453a26f451f5e00a1943
AnshulPatel <patelanshul390@gmail.com>
2024-12-21 12:58:45 +0530

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import HomePage from './landing_page/home/HomePage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <h2>Hii!</h2>
-    </div>
+      <HomePage/>
   </React.StrictMode>
 );
---
+++ src/landing_page/home/Stats.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Stats() {
+    return (<h3>Stats</h3>);
+}
+
+export default Stats;
